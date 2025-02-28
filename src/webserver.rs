34ff4c633b000afdeb48a54ff445c4e791511f31
Xarblu <xarblu@protonmail.com>
2025-02-28 13:02:00 +0100

    add note about rocket address

--- src/webserver.rs
+++ src/webserver.rs
@@ -6,6 +6,8 @@ pub async fn host_file(file: String) -> () {
         .canonicalize().unwrap();
 
     // stable rocket doesn't support hosting individual files yet
+    // TODO should listen on all addresses by default since we talk across namespaces
+    // for now requires ROCKET_ADDRESS=0.0.0.0
     let dir_name = path.parent().unwrap();
     let server = FileServer::from(dir_name);
     let _ = rocket::build()
