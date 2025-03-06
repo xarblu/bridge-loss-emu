1ca59959d6c41f04a3ad7b7a18bbf52a50445fc4
Xarblu <xarblu@protonmail.com>
2025-03-06 11:59:52 +0100

    working netem qdisc

--- src/rtnetlink_utils.rs
+++ src/rtnetlink_utils.rs
@@ -1,6 +1,7 @@
-use rtnetlink::Handle;
-use netlink_packet_utils::traits::Emitable;
-use netlink_packet_route::link::LinkMessageBuffer;
+use rtnetlink::{Handle, QDiscNewRequest};
+use netlink_packet_utils::{byteorder::{ByteOrder, NativeEndian}, nla::DefaultNla, traits::Emitable};
+use netlink_packet_route::{link::LinkMessageBuffer, tc::{TcAttribute, TcHandle, TcMessage, TcOption}};
+use netlink_packet_core::NLM_F_REQUEST;
 use futures::TryStreamExt;
 
 /**
@@ -22,7 +23,6 @@ pub async fn get_interface_id_by_name(handle: Handle, name: String) -> Result<u3
         let mut buf = vec![0; msg.buffer_len()];
         msg.emit(&mut buf);
         let msgbuf = LinkMessageBuffer::new(buf);
-        println!("{}", msgbuf.link_index());
         link_index = Some(msgbuf.link_index());
     }
 
@@ -32,3 +32,74 @@ pub async fn get_interface_id_by_name(handle: Handle, name: String) -> Result<u3
     Ok(link_index.unwrap())
 }
 
+
+// from /usr/include/linux/pkt_sched.h
+// const TCA_NETEM_CORR: u16 = 1;
+// const TCA_NETEM_DELAY_DIST: u16 = 2;
+// const TCA_NETEM_REORDER: u16 = 3;
+// const TCA_NETEM_CORRUPT: u16 = 4;
+const TCA_NETEM_LOSS: u16 = 5;
+const TCA_NETEM_RATE: u16 = 6;
+// const TCA_NETEM_ECN: u16 = 7;
+// const TCA_NETEM_RATE64: u16 = 8;
+// const TCA_NETEM_PAD: u16 = 9;
+// const TCA_NETEM_LATENCY64: u16 = 10;
+// const TCA_NETEM_JITTER64: u16 = 11;
+// const TCA_NETEM_SLOT: u16 = 12;
+// const TCA_NETEM_SLOT_DIST: u16 = 13;
+// const TCA_NETEM_PRNG_SEED: u16 = 14;
+
+pub async fn replace_interface_qdisc_netem(
+    handle: Handle,
+    interface_id: u32,
+    loss: u32,
+    
+) -> Result<(), String> {
+    let mut request = handle
+        .qdisc()
+        .replace(interface_id as i32)
+        .root();
+
+    request.message_mut().attributes.push(
+        TcAttribute::Kind(String::from("netem")));
+
+
+    // add options
+    let mut options: Vec<TcOption> = Vec::new();
+
+    // tc_netem_qopts 
+    let kind = 0;
+    let mut value = vec![0; 20];
+    //__u32	latency;	/* added delay (us) */
+    // 1st in tc_netem_qopts struct but not parsed?
+    //__u32   limit;		/* fifo limit (packets) */
+    NativeEndian::write_u32(&mut value[0..4], 0 as u32);
+    //__u32	loss;		/* random packet loss (0=none ~0=100%) */
+    NativeEndian::write_u32(&mut value[4..8],
+        (u32::MAX as f32 * (loss as f32 / 100.0)) as u32);
+    //__u32	gap;		/* re-ordering gap (0 for none) */
+    NativeEndian::write_u32(&mut value[8..12], 0 as u32);
+    //__u32   duplicate;	/* random packet dup  (0=none ~0=100%) */
+    NativeEndian::write_u32(&mut value[12..16], 0 as u32);
+    //__u32	jitter;		/* random jitter in latency (us) */
+    // skewed by like << 6 bitshift or sth idk
+    NativeEndian::write_u32(&mut value[16..20], 0 as u32);
+    options.push(TcOption::Other(DefaultNla::new(kind, value)));
+
+    // tc_netem_rate
+    let kind = TCA_NETEM_RATE;
+    let mut value = vec![0; 16];
+    //__u32	rate;	/* byte/s */
+    NativeEndian::write_u32(&mut value[0..4], 3000 as u32);
+    //__s32	packet_overhead;
+    //__u32	cell_size;
+    //__s32	cell_overhead;
+    options.push(TcOption::Other(DefaultNla::new(kind, value)));
+
+    request.message_mut().attributes.push(TcAttribute::Options(options));
+
+    let _ = request.execute().await.map_err(|e| e.to_string())?;
+
+    println!("Successfully set qdisc");
+    Ok(())
+}
