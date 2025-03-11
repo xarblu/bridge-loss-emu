73d4bdd0e71eb42ee53104cdd83c4302810840b9
Xarblu <xarblu@protonmail.com>
2025-03-11 14:29:32 +0100

    rewrite trace praser

--- src/trace.rs
+++ src/trace.rs
@@ -1,21 +1,120 @@
 use csv::Reader;
 use std::fs::File;
 use std::str::FromStr;
-use std::process::exit;
-use std::time::Duration;
 
 use crate::rtnetlink_utils::get_interface_id_by_name;
 use crate::rtnetlink_utils::qdisc_netem;
 use crate::rtnetlink_utils::get_distribution;
 
-/// relative_time entry in the trace csv
-const CSV_IDX_REL_TIME: usize = 0;
 
-/// loss entry in the trace csv
-const CSV_IDX_LOSS: usize = 1;
+struct TraceEvent {
+    timestamp: f32,
+    loss: u32
+}
+
+impl TraceEvent {
+    pub fn new(timestamp: f32, loss: u32) -> Self {
+        Self { timestamp, loss }
+    }
+}
+
+struct Trace {
+    trace: Vec<TraceEvent>
+}
+
+impl Trace {
+    pub fn new(rdr: &mut csv::Reader<File>) -> Result<Self, String> {
+        // indices from csv line
+        const CSV_IDX_TIMESTAMP: usize = 0;
+        const CSV_IDX_TIME_UNDER_BRIDGE: usize = 1;
+        const CSV_IDX_SPEED: usize = 2;
+
+        let mut trace: Vec<TraceEvent> = Vec::new();
+        
+        let mut iter = rdr.records();
+        let mut line = 1; // start at 1 due to header
+        while let Some(result) = iter.next() {
+            line += 1;
+            let record = result.unwrap();
+            let timestamp =
+                f32::from_str(&record[CSV_IDX_TIMESTAMP])
+                .map_err(|_| format!("Could not parse f32 from: {} on line {}",
+                        String::from(&record[CSV_IDX_TIMESTAMP]), line))?;
+            let time_under_bridge =
+                f32::from_str(&record[CSV_IDX_TIME_UNDER_BRIDGE])
+                .map_err(|_| format!("Could not parse f32 from: {} on line {}",
+                        String::from(&record[CSV_IDX_TIME_UNDER_BRIDGE]), line))?;
+            let _speed =
+                f32::from_str(&record[CSV_IDX_SPEED])
+                .map_err(|_| format!("Could not parse f32 from: {} on line {}",
+                        String::from(&record[CSV_IDX_SPEED]), line))?;
+            
+            if time_under_bridge > 0.0f32 {
+                // bridge start
+                trace.push(TraceEvent::new(timestamp, 100));
+                // bridge end
+                trace.push(TraceEvent::new(timestamp + time_under_bridge, 0));
+            }
+        }
+
+        Ok(Self { trace })
+    }
+
+    pub async fn run(
+        &mut self,
+        distribution_file: Option<String>,
+        interface: String
+    ) -> Result<(), String> {
+        // setup handle and connection for rtnetlink stuff
+        let (connection, handle, _) = rtnetlink::new_connection().unwrap();
+        tokio::spawn(connection);
+
+        let distribution = get_distribution(
+            distribution_file.unwrap_or(String::from("/lib64/tc/pareto.dist")))
+            .await
+            .expect("Failed to get distribution data");
+
+        // get interface ids
+        let if_id = get_interface_id_by_name(handle.clone(), interface.clone())
+            .await.unwrap();
+
+        // initial state
+        qdisc_netem(
+            handle.clone(),
+            if_id,
+            false,
+            1000,
+            0,
+            37_500_000, // 300 mbit/s
+            50_000_000, // 50 ms
+            10_000_000,  // 10 ms
+            distribution.clone()
+        ).await.unwrap();
+
+        let start = tokio::time::Instant::now();
+        let mut iter = self.trace.iter();
+        while let Some(event) = iter.next() {
+            let timestamp = tokio::time::Duration::from_secs_f32(event.timestamp);
+            let _ = tokio::time::sleep_until(start + timestamp).await;
+            let _ = qdisc_netem(
+                handle.clone(),
+                if_id,
+                true,
+                1000,
+                event.loss,
+                37_500_000, // 300 mbit/s
+                50_000_000, // 50 ms
+                10_000_000,  // 10 ms
+                distribution.clone()
+            ).await.unwrap();
+        }
+        
+        Ok(())
+    }
+}
 
 /**
- * Start playback of the loss trace
+ * Convenence function to create and run a trace
  * @param rdr        csv::Reader for the trace
  * @param interface  Name of the interface the trace should run on
  */ 
@@ -24,88 +123,6 @@ pub async fn run_trace(
     distribution_file: Option<String>,
     interface: String
 ) {
-    // setup handle and connection for rtnetlink stuff
-    let (connection, handle, _) = rtnetlink::new_connection().unwrap();
-    tokio::spawn(connection);
-
-    let distribution = get_distribution(
-        distribution_file.unwrap_or(String::from("/lib64/tc/pareto.dist")))
-        .await
-        .expect("Failed to get distribution data");
-
-    // get interface ids
-    let if_id = get_interface_id_by_name(handle.clone(), interface.clone())
-        .await.unwrap();
-
-    // initial state
-    qdisc_netem(
-        handle.clone(),
-        if_id,
-        false,
-        1000,
-        0,
-        37_500_000, // 300 mbit/s
-        50_000_000, // 50 ms
-        10_000_000,  // 10 ms
-        distribution.clone()
-    ).await.unwrap();
-
-    // cycle through each entry in the csv and toggle netem accordingly
-    let mut iter = rdr.records().peekable();
-    let mut line = 1; // start at 1 due to header
-    while let Some(result) = iter.next() {
-        let record = result.unwrap();
-        line += 1;
-        let relative_time = f32::from_str(&record[CSV_IDX_REL_TIME])
-            .unwrap_or_else(|_| {
-                eprintln!("Could not parse f32 from: {} on line {}",
-                    String::from(&record[CSV_IDX_REL_TIME]), line);
-                exit(1);
-            });
-        let lost = &record[CSV_IDX_LOSS];
-
-        let _ = match lost.into() {
-            "True" => qdisc_netem(
-                        handle.clone(),
-                        if_id,
-                        true,
-                        1000,
-                        100,
-                        37_500_000, // 300 mbit/s
-                        50_000_000, // 50 ms
-                        10_000_000,  // 10 ms
-                        distribution.clone()
-                    ).await,
-            "False" => qdisc_netem(
-                        handle.clone(),
-                        if_id,
-                        true,
-                        1000,
-                        0,
-                        37_500_000, // 300 mbit/s
-                        50_000_000, // 50 ms
-                        10_000_000,  // 10 ms
-                        distribution.clone()
-                    ).await,
-            _ => {
-                eprintln!("Could not parse True/False from: {}", lost);
-                exit(1);
-            }
-        };
-        
-        // check if there's another entry and wait until its timestamp
-        if iter.peek().is_some() {
-            let next_result = iter.peek().unwrap();
-            let next_record = next_result.as_ref().unwrap();
-            let next_relative_time = f32::from_str(&next_record[CSV_IDX_REL_TIME])
-                .unwrap_or_else(|_| {
-                    eprintln!("Could not parse f32 from: {} on line {}",
-                        &next_record[CSV_IDX_REL_TIME], line + 1);
-                    exit(1);
-                });
-            let time_to_wait = next_relative_time - relative_time;
-            println!("{time_to_wait}s until next");
-            std::thread::sleep(Duration::from_secs_f32(time_to_wait));
-        }
-    }
+    let mut trace = Trace::new(rdr).unwrap();
+    let _ = trace.run(distribution_file, interface).await;
 }
